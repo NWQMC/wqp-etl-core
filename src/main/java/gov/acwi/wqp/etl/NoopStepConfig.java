3f6b68f6ec9a2cbafd4ef3560606c61e310d46ca
David Steinich <drsteini@contractor.usgs.gov>
2019-03-28 16:02:19 -0500

    Rename config class to avoid collision

--- src/main/java/gov/acwi/wqp/etl/NoopStepConfig.java
+++ src/main/java/gov/acwi/wqp/etl/NoopStepConfig.java
@@ -7,7 +7,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-public class NoopStep {
+public class NoopStepConfig {
 
 	@Autowired
 	private StepBuilderFactory stepBuilderFactory;
