3a844704b1e3f721d06de8a9a48e3b2aed02d8ba
David Steinich <drsteini@contractor.usgs.gov>
2019-09-06 14:59:08 -0500

    address codacy and revert to earlier travis config

--- src/main/java/gov/acwi/wqp/etl/activityObject/ActivityObject.java
+++ src/main/java/gov/acwi/wqp/etl/activityObject/ActivityObject.java
@@ -1,74 +1,24 @@
 package gov.acwi.wqp.etl.activityObject;
 
-public class ActivityObject {
+import gov.acwi.wqp.etl.BlobObject;
+
+public class ActivityObject extends BlobObject {
 
 	public static final String BASE_TABLE_NAME = "activity_object";
 
-	private Integer dataSourceId;
-	private Integer objectId;
-	private String dataSource;
 	private Integer activityId;
-	private String organization;
 	private String activity;
-	private String objectName;
-	private String objectType;
-	private byte[] objectContent;
 
-	public Integer getDataSourceId() {
-		return dataSourceId;
-	}
-	public void setDataSourceId(Integer dataSourceId) {
-		this.dataSourceId = dataSourceId;
-	}
-	public Integer getObjectId() {
-		return objectId;
-	}
-	public void setObjectId(Integer objectId) {
-		this.objectId = objectId;
-	}
-	public String getDataSource() {
-		return dataSource;
-	}
-	public void setDataSource(String dataSource) {
-		this.dataSource = dataSource;
-	}
 	public Integer getActivityId() {
 		return activityId;
 	}
 	public void setActivityId(Integer activityId) {
 		this.activityId = activityId;
 	}
-	public String getOrganization() {
-		return organization;
-	}
-	public void setOrganization(String organization) {
-		this.organization = organization;
-	}
 	public String getActivity() {
 		return activity;
 	}
 	public void setActivity(String activity) {
 		this.activity = activity;
-	}
-	public String getObjectName() {
-		return objectName;
-	}
-	public void setObjectName(String objectName) {
-		this.objectName = objectName;
-	}
-	public String getObjectType() {
-		return objectType;
-	}
-	public void setObjectType(String objectType) {
-		this.objectType = objectType;
-	}
-	public byte[] getObjectContent() {
-		return objectContent;
-	}
-	public void setObjectContent(byte[] objectContent) {
-		this.objectContent = objectContent;
-	}
-	public static String getBaseTableName() {
-		return BASE_TABLE_NAME;
 	}
 }
--- src/main/java/gov/acwi/wqp/etl/monitoringLocationObject/MonitoringLocationObject.java
+++ src/main/java/gov/acwi/wqp/etl/monitoringLocationObject/MonitoringLocationObject.java
@@ -1,43 +1,14 @@
 package gov.acwi.wqp.etl.monitoringLocationObject;
 
-public class MonitoringLocationObject {
+import gov.acwi.wqp.etl.BlobObject;
+
+public class MonitoringLocationObject extends BlobObject {
 
 	public static final String BASE_TABLE_NAME = "station_object";
 
-	private Integer dataSourceId;
-	private Integer objectId;
-	private String dataSource;
-	private String organization;
 	private Integer stationId;
 	private String siteId;
-	private String objectName;
-	private String objectType;
-	private byte[] objectContent;
 
-	public Integer getDataSourceId() {
-		return dataSourceId;
-	}
-	public void setDataSourceId(Integer dataSourceId) {
-		this.dataSourceId = dataSourceId;
-	}
-	public Integer getObjectId() {
-		return objectId;
-	}
-	public void setObjectId(Integer objectId) {
-		this.objectId = objectId;
-	}
-	public String getDataSource() {
-		return dataSource;
-	}
-	public void setDataSource(String dataSource) {
-		this.dataSource = dataSource;
-	}
-	public String getOrganization() {
-		return organization;
-	}
-	public void setOrganization(String organization) {
-		this.organization = organization;
-	}
 	public Integer getStationId() {
 		return stationId;
 	}
@@ -49,26 +20,5 @@ public class MonitoringLocationObject {
 	}
 	public void setSiteId(String siteId) {
 		this.siteId = siteId;
-	}
-	public String getObjectName() {
-		return objectName;
-	}
-	public void setObjectName(String objectName) {
-		this.objectName = objectName;
-	}
-	public String getObjectType() {
-		return objectType;
-	}
-	public void setObjectType(String objectType) {
-		this.objectType = objectType;
-	}
-	public byte[] getObjectContent() {
-		return objectContent;
-	}
-	public void setObjectContent(byte[] objectContent) {
-		this.objectContent = objectContent;
-	}
-	public static String getBaseTableName() {
-		return BASE_TABLE_NAME;
 	}
 }
