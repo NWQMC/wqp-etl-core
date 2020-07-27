932c654f7e31a48c818576a19bc13bdc59429028
David Steinich <drsteini@contractor.usgs.gov>
2020-07-27 07:47:05 -0500

    update spring boot parent

--- src/main/java/gov/acwi/wqp/etl/FunctionCallTasklet.java
+++ src/main/java/gov/acwi/wqp/etl/FunctionCallTasklet.java
@@ -1,5 +1,6 @@
 package gov.acwi.wqp.etl;
 
+import java.sql.Types;
 import java.util.HashMap;
 
 import org.springframework.batch.core.StepContribution;
@@ -7,6 +8,7 @@ import org.springframework.batch.core.scope.context.ChunkContext;
 import org.springframework.batch.core.step.tasklet.Tasklet;
 import org.springframework.batch.repeat.RepeatStatus;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.SqlParameter;
 import org.springframework.jdbc.core.simple.SimpleJdbcCall;
 
 public abstract class FunctionCallTasklet implements Tasklet {
@@ -31,12 +33,18 @@ public abstract class FunctionCallTasklet implements Tasklet {
 	public RepeatStatus execute(StepContribution contribution, ChunkContext chunkContext) throws Exception {
 		SimpleJdbcCall call = new SimpleJdbcCall(jdbcTemplate)
 				.withSchemaName(wqpSchemaName)
-				.withFunctionName(getFunctionName());
+				.withoutProcedureColumnMetaDataAccess()
+				.withFunctionName(getFunctionName())
+				.declareParameters(
+						new SqlParameter("wqp_data_source", Types.VARCHAR),
+						new SqlParameter("wqp_schema_name", Types.VARCHAR),
+						new SqlParameter("base_table_name", Types.VARCHAR)
+						);
 		HashMap<String, Object> params = new HashMap<String, Object>();
 		params.put("wqp_data_source", wqpDataSource);
 		params.put("wqp_schema_name", wqpSchemaName);
 		params.put("base_table_name", getBaseTableName());
-		call.execute(params);
+		call.executeFunction(String.class, params);
 		return RepeatStatus.FINISHED;
 	}
 }
