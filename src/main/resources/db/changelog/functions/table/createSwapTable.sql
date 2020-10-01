2b291caf7b48d8d305c7ab94ae36406a841c4511
eorosz <eorosz@contractor.usgs.gov>
2020-10-01 13:06:38 -0400

    WQP-1592 (Drop "unlogged" keyword from all WQP tables)   Updated the creation of the swap tables to not be logged.

--- src/main/resources/db/changelog/functions/table/createSwapTable.sql
+++ src/main/resources/db/changelog/functions/table/createSwapTable.sql
@@ -5,7 +5,7 @@ as $$
 declare
     swap_table_name varchar := base_table_name || '_swap_' || wqp_data_source;
 begin
-    execute format('create unlogged table if not exists %I.%I
+    execute format('create table if not exists %I.%I
                     (like %I.%I including comments including defaults including identity including constraints) with (fillfactor = 100)',
         wqp_schema_name, swap_table_name, wqp_schema_name, base_table_name);
 end
